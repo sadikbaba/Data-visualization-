c2c0c7fb299796e91112ca9a24a3b0a6987a3d1a
Abubakar Baba <4821saddikhady@gmail.com>
2026-07-01 21:32:00 +0300

    initial commit

--- mpl_squre.py
+++ mpl_squre.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+plt.plot(input_values, squares, linewidth=5)
 
-squares = [2, 4, 9, 16, 25]
-plt.plot(squares, linewidth=5)
 # Set chart title and label axes.
 
-plt.title("Square Numbers", fontsize=24)
+plt.title("Square Numbers", fontsize=14)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Value", fontsize=14)
 
